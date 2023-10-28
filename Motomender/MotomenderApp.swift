df2c81f20610a1ec91923e8b7bbdc797b1e8d31d
Catalin Albu <catalin.albu@exigea.ro>
2023-10-28 11:52:30 +0300

    Initial Commit

---
+++ Motomender/MotomenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotomenderApp.swift
+//  Motomender
+//
+//  Created by Catalin Albu on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MotomenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
